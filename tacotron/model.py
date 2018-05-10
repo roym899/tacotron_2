705387a9c6e2f5f1ed6ed7cf86553451f3e6c25d
Leonard Bruns <roym899@gmail.com>
2018-05-10 18:45:14 +0200

    Add basic model structure

---
+++ tacotron/model.py
@@ -0,0 +1,68 @@
+import tensorflow as tf
+
+class TTS(object):
+    def __init__(self, hparams, mode, dataset_iterator):
+        if mode == "basic":
+            # embedding -> LSTM layer -> LSTM layer -> Dense
+            # -----------------------    -------------------
+            #         Encoder               Decoder
+
+            # define the input
+            encoder_inputs = tf.placeholder(tf.int32, [1, hparams['max_sentence_length']], 'inputs')
+
+            # Embedding
+            embedding_encoder = tf.get_variable(
+                "embedding_encoder", [hparams['src_vocab_size'], hparams['embedding_size']])
+            # Look up embedding:
+            #   encoder_inputs: [max_time, batch_size]
+            #   encoder_emb_inp: [max_time, batch_size, embedding_size]
+            encoder_emb_inp = tf.nn.embedding_lookup(embedding_encoder, encoder_inputs)
+
+            # Build RNN cell
+            encoder_cell = tf.nn.rnn_cell.BasicLSTMCell(hparams['basic_encoder_lstm_cells'])
+            # Run Dynamic RNN
+            #   encoder_outputs: [max_time, batch_size, num_units]
+            #   encoder_state: [batch_size, num_units]
+            encoder_outputs, encoder_state = tf.nn.dynamic_rnn(encoder_cell,
+                                                               encoder_emb_inp,
+                                                               sequence_length=hparams['max_sentence_length'],
+                                                               time_major=True)
+
+            # Build RNN cell
+            # Helper
+            decoder_inputs = tf.placeholder(tf.float64,
+                                            [hparams['frequency_bins'], hparams['max_output_length']],
+                                            'inputs')
+
+            helper = tf.contrib.seq2seq.TrainingHelper(decoder_inputs, hparams['max_output_length'], time_major=True)
+            # Decoder
+            decoder_cell = tf.nn.rnn_cell.BasicLSTMCell(hparams['basic_encoder_lstm_cells'])
+            projection_layer = tf.layers.Dense(hparams['frequency_bins'], use_bias=False)
+            decoder = tf.contrib.seq2seq.BasicDecoder(decoder_cell,
+                                                      helper,
+                                                      encoder_state,
+                                                      output_layer=projection_layer)
+            # Dynamic decoding
+            outputs, _ = tf.contrib.seq2seq.dynamic_decode(decoder)
+            spectograms = outputs.rnn_output
+
+            target_spectograms = dataset_iterator.target_spectograms
+            train_loss = tf.losses.mean_squared_error(target_spectograms, spectograms)
+            params = tf.trainable_variables()
+            gradients = tf.gradients(train_loss,
+                                     params)
+            clipped_gradients, _ = tf.clip_by_global_norm(gradients, hparams['max_gradient_norm'])
+
+            # Optimization
+            optimizer = tf.train.AdamOptimizer(hparams['learning_rate'])
+            update_step = optimizer.apply_gradients((clipped_gradients, params))
+
+        elif mode == "tacotron-2":
+            pass
+
+
+    def train(self):
+        pass
+
+    def predict(self):
+        pass
--- tacotron_2.py
+++ tacotron_2.py
@@ -1,8 +1,23 @@
 # Tacotron 2 Main File
 import dataset
+from tacotron.model import TTS
 
 # assuming the dataset folder structure is
 # DATASET_PATH/wavn/*.wav
 # DATASET_PATH/prompts.data
 DATASET_PATH = "../dataset/"
 
+# define the basic vocabulary
+VOCAB = "ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz -?!.,;:\'"
+
+hparams = {}
+hparams['src_vocab_size'] = len(VOCAB)
+hparams['embedding_size'] = 512
+hparams['max_sentence_length'] = 50
+hparams['basic_encoder_lstm_cells'] = 256
+hparams['frequency_bins'] = 2048
+hparams['max_output_length'] = 5000
+hparams['max_gradient_norm'] = 5
+hparams['learning_rate'] = 0.0001
+
+improved_tacotron_2_model = TTS(hparams, "basic")
